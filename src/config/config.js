d397dc58d789218e39c288a4abc939f6a3ae76b8
jeet sehgal <jeetsehgal786@gmail.com>
2025-12-03 19:20:23 +0530

    removed the bucket

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,6 @@ const config={
     backend_project_id:String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
     backend_database_id:String(import.meta.env.VITE_APPWRITE_DATABASE),
     backend_password_table:String(import.meta.env.VITE_APPWRITE_PASSWORD_TABLE),
-    backend_profile_img_bucket:String(import.meta.env.VITE_APPWRITE_PROFILE_IMG_BUCKET),
     backend_secret_key:String(import.meta.env.VITE_SECRET_KEY),
     
 }
